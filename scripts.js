ddbc5f549a39eebe1ad382f25728fdaebb99a4ba
chishokie <lumi.siri03@gmail.com>
2026-04-15 20:47:15 +0300

    Poistettu turha js koodi

--- scripts.js
+++ scripts.js
@@ -1,7 +1,5 @@
 function setTime() {
   const now = new Date();
-  
-  const time = now.toLocaleTimeString('fi-FI');
 
   const dateOptions = {
     weekday: 'short',
@@ -11,7 +9,7 @@ function setTime() {
   };
   const date = now.toLocaleDateString('fi-FI', dateOptions);
 
-  const datetime = `<p>Tänään on ${date}</p><p>Kello on ${time}</p>`;
+  const datetime = `<p>Tänään on ${date}</p>`;
 
   document.getElementById("datetime").innerHTML = datetime;
 }
